d3d77bcddd4d467836a80dcc9eec5a642f15f160
Ozzie <57572826+ozzman84@users.noreply.github.com>
2022-01-17 21:46:27 -0700

    Add subscriptions routes to add index create and update

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      namespace :customers do
+        resources :subscriptions, only: %i[index create update]
+      end
+    end
+  end
 end
